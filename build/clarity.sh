c30ac8d8bbf679f17d10f2a0d2950b8114bc7a8b
Daison Carino <daison12006013@gmail.com>
2016-02-03 14:49:15 +0000

    updating sh file

--- build/clarity.sh
+++ build/clarity.sh
@@ -1,7 +1,7 @@
 git subsplit init git@github.com:ps-clarity/framework.git
-git subsplit publish --heads="master dev" src/Clarity:git@github.com:ps-clarity/kernel.git
-git subsplit publish --heads="master dev" src/Clarity/View:git@github.com:ps-clarity/view.git
-git subsplit publish --heads="master dev" src/Clarity/Support:git@github.com:ps-clarity/support.git
-git subsplit publish --heads="master dev" src/Clarity/Contracts:git@github.com:ps-clarity/contracts.git
-git subsplit publish --heads="master dev" src/Clarity/Exceptions:git@github.com:ps-clarity/exceptions.git
+git subsplit publish --heads="master" src/Clarity:git@github.com:ps-clarity/kernel.git
+git subsplit publish --heads="master" src/Clarity/View:git@github.com:ps-clarity/view.git
+git subsplit publish --heads="master" src/Clarity/Support:git@github.com:ps-clarity/support.git
+git subsplit publish --heads="master" src/Clarity/Contracts:git@github.com:ps-clarity/contracts.git
+git subsplit publish --heads="master" src/Clarity/Exceptions:git@github.com:ps-clarity/exceptions.git
 rm -rf .subsplit/
