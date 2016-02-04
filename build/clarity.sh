cae7ab0d0fd855060200a70528f20df025bc16ce
Daison Carino <daison12006013@gmail.com>
2016-02-04 03:22:54 +0000

    removing phpunit config

--- build/clarity.sh
+++ build/clarity.sh
@@ -1,8 +1,8 @@
 git subsplit init git@github.com:phalconslayer/framework.git
+git subsplit publish --heads="master dev" src/Clarity/Console:git@github.com:ps-clarity/console.git
 git subsplit publish --heads="master dev" src/Clarity:git@github.com:ps-clarity/kernel.git
 git subsplit publish --heads="master dev" src/Clarity/View:git@github.com:ps-clarity/view.git
 git subsplit publish --heads="master dev" src/Clarity/Support:git@github.com:ps-clarity/support.git
 git subsplit publish --heads="master dev" src/Clarity/Contracts:git@github.com:ps-clarity/contracts.git
 git subsplit publish --heads="master dev" src/Clarity/Exceptions:git@github.com:ps-clarity/exceptions.git
-git subsplit publish --heads="master dev" src/Clarity/Console:git@github.com:ps-clarity/console.git
 rm -rf .subsplit/
