0b02ee76b09ed24e8d1b1496259c875188fb8727
Daison Carino <daison12006013@gmail.com>
2016-02-04 04:25:59 +0000

    linking the test suite

---
+++ tests/slayer/config/AppTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Slayer\Config;
+
+class AppTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $GLOBALS['kernel']->modules()->run('main');
+    }
+
+    public function testAppFile()
+    {
+        $this->assertContains(config()->app->debug, [
+            true,
+            false,
+        ]);
+    }
+}
