bde4f687b5679cf80838f71b97bc0f88184b5afe
Daison Carino <daison12006013@gmail.com>
2016-02-23 15:36:19 +0000

    updating the build

--- build/clarity.sh
+++ build/clarity.sh
@@ -1,10 +1,13 @@
 git subsplit init git@github.com:phalconslayer/framework.git
-git subsplit publish --heads="master dev" src/Clarity/Lang:git@github.com:ps-clarity/lang.git
-git subsplit publish --heads="master dev" src/Clarity/Mail:git@github.com:ps-clarity/mail.git
 git subsplit publish --heads="master dev" src/Clarity/Console:git@github.com:ps-clarity/console.git
-git subsplit publish --heads="master dev" src/Clarity:git@github.com:ps-clarity/kernel.git
-git subsplit publish --heads="master dev" src/Clarity/View:git@github.com:ps-clarity/view.git
-git subsplit publish --heads="master dev" src/Clarity/Support:git@github.com:ps-clarity/support.git
 git subsplit publish --heads="master dev" src/Clarity/Contracts:git@github.com:ps-clarity/contracts.git
 git subsplit publish --heads="master dev" src/Clarity/Exceptions:git@github.com:ps-clarity/exceptions.git
+#git subsplit publish --heads="master dev" src/Clarity/Facades:git@github.com:ps-clarity/facades.git
+#git subsplit publish --heads="master dev" src/Clarity/Kernel:git@github.com:ps-clarity/kernel.git
+git subsplit publish --heads="master dev" src/Clarity/Lang:git@github.com:ps-clarity/lang.git
+git subsplit publish --heads="master dev" src/Clarity/Mail:git@github.com:ps-clarity/mail.git
+#git subsplit publish --heads="master dev" src/Clarity/Providers:git@github.com:ps-clarity/providers.git
+git subsplit publish --heads="master dev" src/Clarity/Support:git@github.com:ps-clarity/support.git
+git subsplit publish --heads="master dev" src/Clarity/View:git@github.com:ps-clarity/view.git
+
 rm -rf .subsplit/
