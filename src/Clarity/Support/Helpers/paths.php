98f8a039953a84a5e58102c73e72c97e1f5d53d8
Daison Carino <daison12006013@gmail.com>
2016-02-28 16:00:15 +0000

    adding url_trimmer() on every path builder

--- src/Clarity/Support/Helpers/paths.php
+++ src/Clarity/Support/Helpers/paths.php
@@ -3,56 +3,56 @@
 if (!function_exists('base_path')) {
     function base_path($extend_path = null)
     {
-        return config()->path->root . $extend_path;
+        return url_trimmer(config()->path->root.'/'.$extend_path);
     }
 }
 
 if (!function_exists('app_path')) {
     function app_path($extend_path = null)
     {
-        return config()->path->app . $extend_path;
+        return url_trimmer(config()->path->app.'/'.$extend_path);
     }
 }
 
 if (!function_exists('config_path')) {
     function config_path($extend_path = null)
     {
-        return config()->path->config . $extend_path;
+        return url_trimmer(config()->path->config.'/'.$extend_path);
     }
 }
 
 if (!function_exists('database_path')) {
     function database_path($extend_path = null)
     {
-        return config()->path->database . $extend_path;
+        return url_trimmer(config()->path->database.'/'.$extend_path);
     }
 }
 
 if (!function_exists('storage_path')) {
     function storage_path($extend_path = null)
     {
-        return config()->path->storage . $extend_path;
+        return url_trimmer(config()->path->storage.'/'.$extend_path);
     }
 }
 
 if (!function_exists('public_path')) {
     function public_path($extend_path = null)
     {
-        return config()->path->public . $extend_path;
+        return url_trimmer(config()->path->public.'/'.$extend_path);
     }
 }
 
 if (!function_exists('resources_path')) {
     function resources_path($extend_path = null)
     {
-        return config()->path->resources . $extend_path;
+        return url_trimmer(config()->path->resources.'/'.$extend_path);
     }
 }
 
 if (!function_exists('sandbox_path')) {
     function sandbox_path($extend_path = null)
     {
-        return config()->path->sandbox . $extend_path;
+        return url_trimmer(config()->path->sandbox.'/'.$extend_path);
     }
 }
 
@@ -75,27 +75,27 @@ if (!function_exists('cp')) {
         foreach ($iterator as $item) {
 
             # check if the item is directory
-            if ($item->isDir()) {
+            if ( $item->isDir() )
+            {
 
                 # check if there is existing directory
                 # else create.
+
                 $_temp_dir = $dest . '/' . $iterator->getSubPathName();
-                if (is_dir($_temp_dir) === false) {
+
+                if (is_dir($_temp_dir) === false)
+                {
                     mkdir($dest . '/' . $iterator->getSubPathName(), true);
                 }
-            } # else, it is a file
-            else {
+
+            }
+            # it is a file
+            else
+            {
 
                 copy($item, $dest . '/' . $iterator->getSubPathName());
             }
         }
-    }
-}
-
-if (!function_exists('remove_double_slash')) {
-    function remove_double_slash($path)
-    {
-        return str_replace('//', '/', $path);
     }
 }
 
@@ -112,12 +112,15 @@ if (!function_exists('folder_files')) {
         );
 
         $files = [];
-        foreach ($iterator as $item) {
-            if ($item->isDir()) {
+
+        foreach ($iterator as $item)
+        {
+            if ($item->isDir())
+            {
                 continue;
             }
 
-            $files[] = $iterator->getPathName();
+            $files[] = $item->getPathName();
         }
 
         return $files;
