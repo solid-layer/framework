c1e4a22a6913c533247a4301f97f4c8742e897c2
Daison Carino <daison12006013@gmail.com>
2016-03-28 08:14:26 +0000

    refactoring controller console command to be extendable to change the stubs/paths itself for issue #6

--- src/Clarity/Console/App/ControllerCommand.php
+++ src/Clarity/Console/App/ControllerCommand.php
@@ -9,87 +9,173 @@ use League\Flysystem\Adapter\Local as LeagueFlysystemAdapterLocal;
 
 class ControllerCommand extends SlayerCommand
 {
-    protected $name        = 'app:controller';
+    protected $name = 'app:controller';
     protected $description = 'Generate a new controller';
 
-    private $app;
-
-    public function __construct()
+    /**
+     * Get the application path
+     *
+     * @return string
+     */
+    protected function getAppPath()
     {
-        $this->app = new Filesystem(
-            new LeagueFlysystemAdapterLocal(config()->path->app, 0)
-        );
-
-        parent::__construct();
+        return config()->path->app;
     }
 
+    /**
+     * Get the module path
+     *
+     * @return string
+     */
+    protected function getModulePath()
+    {
+        return url_trimmer($this->getAppPath().'/'.$this->getModuleName());
+    }
+
+    /**
+     * Get the base path
+     *
+     * @return string
+     */
+    protected function getBasePath()
+    {
+        return realpath('');
+    }
+
+    /**
+     * @return string
+     */
+    protected function getNamespace()
+    {
+        return url_trimmer(
+            $this->getAppPath().'/'.$this->getModuleName().'\\Controllers'
+        );
+    }
+
+    /**
+     * @return string
+     */
+    protected function getControllerName($prettify = true)
+    {
+        $ret = '%s';
+
+        if ($prettify) {
+            $ret = 'controllers/%sController.php';
+        }
+
+        return sprintf(
+            $ret,
+            studly_case(
+                str_slug($this->input->getArgument('name'), '_')
+            )
+        );
+    }
+
+    /**
+     * Get the module name
+     *
+     * @return string
+     */
+    protected function getModuleName()
+    {
+        return $this->input->getArgument('module');
+    }
+
+    /**
+     * Get the controller content stub
+     *
+     * @return string
+     */
+    protected function getControllerStub()
+    {
+        return file_get_contents(__DIR__.'/stubs/makeController.stub');
+    }
+
+    /**
+     * Get the functions content stub
+     *
+     * @return string
+     */
+    protected function getFunctionsStub()
+    {
+        return file_get_contents(__DIR__.'/stubs/_controllerFunctions.stub');
+    }
+
+    /**
+     * Get the controller functions
+     *
+     * @return string
+     */
+    protected function getControllerFunctions()
+    {
+        if ($this->input->getOption('emptify')) {
+            return '';
+        }
+
+        return stubify(
+            $this->getFunctionsStub(),
+            ['folderName' => $this->input->getArgument('name')]
+        );
+    }
+
+    /**
+     *
+     */
+    private function filesystem($path)
+    {
+        return new Filesystem(
+            new LeagueFlysystemAdapterLocal($path, 0)
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function slash()
     {
-        $app_path = str_replace(BASE_PATH, '', config()->path->app);
-        $arg_name = studly_case(str_slug($this->input->getArgument('name'), '_'));
+        $app_filesystem = $this->filesystem($this->getAppPath());
 
-        # - get the main stub file
+        $module = $this->getModuleName();
 
-        $stub = file_get_contents(__DIR__ . '/stubs/makeController.stub');
-        $stub = stubify($stub, [
-            'controllerName' => $arg_name
-        ]);
-
-
-        # - use emptify
-
-        if ($this->input->getOption('emptify') === true) {
-            $stub = stubify($stub, [
-                'controllerFunctions' => ''
-            ]);
-        } else {
-            $controller_functions = file_get_contents(__DIR__ . '/stubs/_controllerFunctions.stub');
-
-            $stub = stubify($stub, [
-                'controllerFunctions' => $controller_functions,
-                'controllerName'      => strtolower($arg_name)
-            ]);
-        }
-
-
-        # - create the file name
-
-        $file_name = $arg_name . 'Controller.php';
-
-        $module  = $this->input->getArgument('module');
-
-        if ( $this->app->has($module) === false ) {
-            $this->error('Module not found `' . $module . '`');
+        # check if module exists, throw error if it doesn't exists
+        if ($app_filesystem->has($module) === false) {
+            $this->error("Module [$module] not found");
 
             return;
         }
 
-        $controllers = $module . '/controllers/';
-        if ( $this->app->has($controllers) === false ) {
-            $this->error('Controllers folder not found from your module: `' . $module . '`');
+        $controller = $this->getControllerName();
+
+        # check controller file if exists, throw error if exists
+        if ($app_filesystem->has($module.'/'.$controller)) {
+            $this->error(
+                "Controller [{$this->input->getArgument('name')}] ".
+                "already exists in your Module [{$this->input->getArgument('module')}]"
+            );
 
             return;
         }
 
-        $stub = stubify(
-            $stub, [
-                'namespace' => path_to_namespace($app_path . $controllers)
+
+        # get the controller stub and stubify the {controllerName}
+        # based on argument controller name
+        $buff = stubify(
+            $this->getControllerStub(),
+            [
+                'namespace' => path_to_namespace(
+
+                    # here, we must trim the $namespace by
+                    # getting the base path to be our matching trimmer
+                    str_replace($this->getBasePath(), '', $this->getNamespace())
+                ),
+                'controllerName' => $this->getControllerName(false),
+                'controllerFunctions' => $this->getControllerFunctions(),
             ]
         );
 
-
-        $this->comment('Crafting Controller...');
-
-        if ( $this->app->has($file_name) ) {
-            $this->error('   Controller already exists!');
-
-            return;
-        }
-
-        $this->app->put($controllers . $file_name, $stub);
-        $this->info('   ' . $file_name . ' created!');
-
-        $this->callDumpAutoload();
+        # now write the content based on $controller path
+        $module_filesystem = $this->filesystem($this->getModulePath());
+        $module_filesystem->put($controller, $buff);
     }
 
     protected function arguments()
@@ -108,9 +194,8 @@ class ControllerCommand extends SlayerCommand
             [
                 'emptify',
                 null,
-                InputOption::VALUE_OPTIONAL,
+                InputOption::VALUE_NONE,
                 'Remove all pre-defined functions',
-                false,
             ],
         ];
     }
