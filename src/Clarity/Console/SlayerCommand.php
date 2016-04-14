3cd12ec14ab56b268a0deeae2dd1e1be702de839
Daison Carino <daison12006013@gmail.com>
2016-04-14 12:57:04 +0000

    fixing the environment call to use provided environment instead

--- src/Clarity/Console/SlayerCommand.php
+++ src/Clarity/Console/SlayerCommand.php
@@ -23,19 +23,63 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Exception\InvalidOptionException;
 
+/**
+ * An abstract class that handles all slayer console
+ */
 abstract class SlayerCommand extends Command
 {
     use ServiceMagicMethods;
 
+    /**
+     * The input interface instance
+     *
+     * @var mixed
+     */
     protected $input;
+
+    /**
+     * The output interface instance
+     *
+     * @var mixed
+     */
     protected $output;
+
+    /**
+     * The environment option
+     *
+     * if this is marked as true (boolean), it will activate the --env option
+     *
+     * @var boolean
+     */
     protected $environment_option = true;
+
+    /**
+     * The timeout option
+     *
+     * if this is marked as true (boolean), it will activate the --timeout option
+     *
+     * @var boolean
+     */
     protected $timeout_option = true;
 
-    abstract public function slash();
-
+    /**
+     * A default constant timeout
+     */
     const TIMEOUT = 60;
 
+    /**
+     * An abstract function that will be called on every providers
+     *
+     * @return mixed
+     */
+    abstract public function slash();
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param InputInterface  $input  An InputInterface instance
+     * @param OutputInterface $output An OutputInterface instance
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->input = $input;
@@ -48,15 +92,22 @@ abstract class SlayerCommand extends Command
         $this->slash();
     }
 
+    /**
+     * This loads the environment
+     *
+     * @return \Clarity\Console\SlayerCommand
+     */
     protected function loadEnv()
     {
         $env = $this->getInput()->getOption('env');
 
-        config(['environment' => $env]);
-
         $folder = '';
 
-        if ( $env !== 'production') {
+        if ($env !== null) {
+            config(['environment' => $env]);
+        }
+
+        if ($env !== 'production') {
             $folder = $env;
         }
 
@@ -74,6 +125,11 @@ abstract class SlayerCommand extends Command
         return $this;
     }
 
+    /**
+     * This loads the timeout
+     *
+     * @return \Clarity\Console\SlayerCommand
+     */
     protected function loadTimeout()
     {
         $timeout = $this->getInput()->getOption('timeout');
@@ -85,16 +141,29 @@ abstract class SlayerCommand extends Command
         return $this;
     }
 
+    /**
+     * Returns the input interface instance
+     *
+     * @return mixed
+     */
     protected function getInput()
     {
         return $this->input;
     }
 
+    /**
+     * Returns the output interface instance
+     *
+     * @return mixed
+     */
     protected function getOutput()
     {
         return $this->output;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
         $this
@@ -120,7 +189,7 @@ abstract class SlayerCommand extends Command
                 'e',
                 InputOption::VALUE_OPTIONAL,
                 'The environment to be used',
-                'production'
+                null
             );
         }
 
@@ -152,36 +221,76 @@ abstract class SlayerCommand extends Command
         return $this;
     }
 
+    /**
+     * This provides the arguments of this console
+     *
+     * @return mixed
+     */
     protected function arguments()
     {
         return [];
     }
 
+    /**
+     * This provides the options of this console
+     *
+     * @return mixed
+     */
     protected function options()
     {
         return [];
     }
 
-    public function info($string)
+    /**
+     * This prints-out an info colored message
+     *
+     * @param  string $message The message to be printed
+     * @return void
+     */
+    public function info($message)
     {
-        $this->output->writeln("<info>$string</info>");
+        $this->output->writeln("<info>$message</info>");
     }
 
-    public function line($string)
+    /**
+     * This prints-out a line colored message
+     *
+     * @param  string $message The message to be printed
+     * @return void
+     */
+    public function line($message)
     {
-        $this->output->writeln($string);
+        $this->output->writeln($message);
     }
 
-    public function comment($string)
+    /**
+     * This prints-out a comment colored message
+     *
+     * @param  string $message The message to be printed
+     * @return void
+     */
+    public function comment($message)
     {
-        $this->output->writeln("<comment>$string</comment>");
+        $this->output->writeln("<comment>$message</comment>");
     }
 
-    public function error($string)
+    /**
+     * This prints-out an error colored message
+     *
+     * @param  string $message The message to be printed
+     * @return void
+     */
+    public function error($message)
     {
-        $this->output->writeln("<error>$string</error>");
+        $this->output->writeln("<error>$message</error>");
     }
 
+    /**
+     * This handles exception and prints-out error colored message
+     *
+     * @param  \Exception $e
+     * @return void
+     */
     public function exception($e)
     {
         $message = sprintf(
@@ -195,6 +304,13 @@ abstract class SlayerCommand extends Command
         $this->error("\n".$message."\n");
     }
 
+    /**
+     * Ask something with a default value
+     *
+     * @param  string                $message The message to be printed
+     * @param  bool|string|int]mixed $default The default value
+     * @return mixed
+     */
     public function ask($message, $default)
     {
         $helper = $this->getHelper('question');
@@ -204,6 +320,12 @@ abstract class SlayerCommand extends Command
         return $helper->ask($this->input, $this->output, $question);
     }
 
+    /**
+     * To confirm that only works with [y/n]
+     *
+     * @param  string $message The message to be printed
+     * @return mixed
+     */
     public function confirm($message)
     {
         $helper = $this->getHelper('question');
@@ -213,6 +335,11 @@ abstract class SlayerCommand extends Command
         return $helper->ask($this->input, $this->output, $question);
     }
 
+    /**
+     * Call the composer's dumpautoload
+     *
+     * @return void
+     */
     public function callDumpAutoload()
     {
         CLI::bash([
