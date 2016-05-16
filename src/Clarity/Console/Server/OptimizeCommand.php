633e0b98c0afac5bf19563017df378cba709b718
Daison Carino <daison12006013@gmail.com>
2016-05-16 06:05:43 +0000

    refactoring the optimize and adding config file to be used

--- src/Clarity/Console/Server/OptimizeCommand.php
+++ src/Clarity/Console/Server/OptimizeCommand.php
@@ -14,7 +14,10 @@
 namespace Clarity\Console\Server;
 
 use Clarity\Console\CLI;
+use ClassPreloader\Factory;
 use Clarity\Console\SlayerCommand;
+use Symfony\Component\Console\Input\InputOption;
+use ClassPreloader\Exceptions\VisitorExceptionInterface;
 
 /**
  * A console command that optimized the whole system
@@ -37,22 +40,98 @@ class OptimizeCommand extends SlayerCommand
     public function slash()
     {
         $this->callComposerOptimizer();
-        $this->callClassPreloader();
+
+        if (
+            $this->input->getOption('force')
+            // && !config()->app->debug
+        ) {
+            $this->info('Compiling classes...');
+            $this->compileClasses();
+        }
     }
 
+
     /**
-     * This calls the class preloader to combine class files
+     * Generate the compiled class file.
      *
      * @return void
      */
-    protected function callClassPreloader()
+    protected function compileClasses()
     {
-        CLI::bash([
-            'php vendor/classpreloader/console/classpreloader.php compile ' .
-            '--config="bootstrap/compiler.php" ' .
-            '--output="' . config()->path->storage . '/slayer/compiled.php" ' .
-            '--strip_comments=1',
-        ]);
+        $preloader = (new Factory)->create(['skip' => false]);
+
+        $compile_path = config()->path->storage.'/slayer/compiled.php';
+
+        $handle = $preloader->prepareOutput($compile_path);
+
+        foreach ($this->getClassFiles() as $file) {
+
+            try {
+
+                fwrite(
+                    $handle,
+                    $preloader->getCode($file, false)."\n"
+                );
+
+            } catch (VisitorExceptionInterface $e) {
+                //
+            }
+        }
+
+        fclose($handle);
+
+        $this->info('   Compiled at '.url_trimmer($compile_path));
+    }
+
+    private function filesKey($files)
+    {
+        foreach ($files as $idx => $path) {
+            $files[$path] = $path;
+            unset($files[$idx]);
+        }
+
+        return $files;
+    }
+
+    /**
+     * Get the classes that should be combined and compiled.
+     *
+     * @return array
+     */
+    protected function getClassFiles()
+    {
+        $core = $this->filesKey(
+            array_map('url_trimmer', require __DIR__.'/Optimize/config.php')
+        );
+
+        $files = array_merge(
+            $core,
+            $this->filesKey(
+                array_map('url_trimmer', (array) config('compile.files', []))
+            )
+        );
+
+        foreach ((array) config('compile.folders', []) as $folder) {
+            $files = array_merge(
+                $files,
+                $this->filesKey(
+                    array_map('url_trimmer', folder_files($folder, true))
+                )
+            );
+        }
+
+        foreach ((array) config('compile.providers', []) as $provider) {
+            $files = array_merge(
+                $files,
+                $this->filesKey(
+                    array_map('url_trimmer', forward_static_call([$provider, 'compiles']))
+                )
+            );
+        }
+
+        return array_map('url_trimmer',
+            array_map('realpath', $files)
+        );
     }
 
     /**
@@ -66,11 +145,26 @@ class OptimizeCommand extends SlayerCommand
             'composer dumpautoload --optimize',
         ]);
 
-        $this->info('Removing previous compiled file...');
-
         $compiled_file = BASE_PATH . '/storage/slayer/compiled.php';
+
         if ( file_exists($compiled_file) ) {
+            $this->info('Removing previous compiled file...');
             unlink($compiled_file);
         }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function options()
+    {
+        return [
+            [
+                'force',
+                null,
+                InputOption::VALUE_NONE,
+                'To write classes inside compiled.php',
+            ],
+        ];
     }
 }
