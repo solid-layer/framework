358a6269a2a9e61643a56d8636e5993398568bff
Daison Carino <daison12006013@gmail.com>
2016-07-27 09:40:57 +0000

    fixing path

--- build/ci/zephir.sh
+++ build/ci/zephir.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd cphalcon/vendor/phalcon/zephir
+cd ${TRAVIS_BUILD_DIR}/cphalcon/vendor/phalcon/zephir
 
 ZEPHIRDIR="$( cd "$( dirname . )" && pwd )"
 sed "s#%ZEPHIRDIR%#$ZEPHIRDIR#g" bin/zephir > bin/zephir-cmd
