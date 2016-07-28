a5a3f68f6fb8e7a30d439692ec78e9b705a13306
Daison Carino <daison12006013@gmail.com>
2016-07-28 00:26:02 +0000

    moving back the zephir command to yaml file

--- build/ci/zephir.sh
+++ build/ci/zephir.sh
@@ -15,10 +15,4 @@ if [ "$PHALCON_VERSION" != "2.0.x" ]; then
 
     # test zephir
     (cd ${TRAVIS_BUILD_DIR};zephir)
-
-    # zephir load determining php version
-    cd ${TRAVIS_BUILD_DIR}/${PHALCON_FOLDER}
-    [[ "$TRAVIS_PHP_VERSION" == "7.0" ]] || ( zephir fullclean && zephir generate )
-    [[ "$TRAVIS_PHP_VERSION" != "7.0" ]] || ( zephir fullclean && zephir generate --backend=ZendEngine3 )
-
 fi
