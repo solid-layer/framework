8c993c597934c644fa701bf3308fe81fa208030b
Daison Carino <daison12006013@gmail.com>
2016-08-01 00:00:47 +0000

    initially changing the build script to support phalcon-ci-installer package

--- build/ci/build.sh
+++ build/ci/build.sh
@@ -1,36 +1,41 @@
 #!/usr/bin/env bash
 
-# handle slayer installation
-if [ ! -f "${TRAVIS_BUILD_DIR}/${SLAYER_FOLDER}/composer.json" ]; then
+# clone slayer
+composer create-project phalconslayer/slayer:${SLAYER_VERSION} ~/${SLAYER_FOLDER}
 
-    composer create-project phalconslayer/slayer:${SLAYER_VERSION} ${TRAVIS_BUILD_DIR}/${SLAYER_FOLDER}
+cd ~/${SLAYER_FOLDER}
 
-    cd ${TRAVIS_BUILD_DIR}/${SLAYER_FOLDER}
 
-    composer update
+# update composer and slayer's vendor
+php -m
+composer self-update
+composer update
 
-    # run the composer update
-    composer self-update
-    php -m
 
-    cd ..
+# require framework based on version
+composer require phalconslayer/framework:${FRAMEWORK_VERSION}
 
-fi
 
-cd ${TRAVIS_BUILD_DIR}/${SLAYER_FOLDER}
+# execute phalcon ci installer
+vendor/bin/install-phalcon.sh ${PHALCON_VERSION}
+
 
 # copy .env.travis as .env file
 cp vendor/phalconslayer/framework/tests/.env.travis .env
 cat .env
 
+
 # create 'slayer' database
 mysql -e 'create database slayer charset=utf8mb4 collate=utf8mb4_unicode_ci;'
+
 
 # db migrations
 php brood db:migrate
 
+
 # built-in web
 php -S ${SERVE_HOST}:${SERVE_PORT} -t public/ > built-in.log 2>&1 &
 sleep 5
+
 
 cd ..
