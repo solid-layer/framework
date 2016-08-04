8afa3fb3d753e993ac12f88ade7bbddf9aaba06b
Daison Carino <daison12006013@gmail.com>
2016-08-04 04:06:13 +0000

    changing link to use docs.phalconslayer.com [ci skip]

--- src/Clarity/Console/Queue/Listen.php
+++ src/Clarity/Console/Queue/Listen.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * PhalconSlayer\Framework.
+ *
+ * @copyright 2015-2016 Daison Carino <daison12006013@gmail.com>
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://docs.phalconslayer.com
+ */
 namespace Clarity\Console\Queue;
 
 use Exception;
@@ -15,7 +21,7 @@ class Listen extends Brood
     {
         $data = $job->getBody();
 
-        if (!isset($data['class'])) {
+        if (! isset($data['class'])) {
             return false;
         }
 
--- src/Clarity/Support/Phinx/Console/Command/Test.php
+++ src/Clarity/Support/Phinx/Console/Command/Test.php
@@ -4,7 +4,7 @@
  *
  * @copyright 2015-2016 Daison Carino <daison12006013@gmail.com>
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
- * @link      http://phalconslayer.readme.io
+ * @link      http://docs.phalconslayer.com
  */
 
 /**
