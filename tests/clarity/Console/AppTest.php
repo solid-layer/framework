496402bd95fe6993fc4a531bd6f1709f71b70f9a
Daison Carino <daison12006013@gmail.com>
2016-09-06 03:36:19 +0000

    removing condition

--- tests/clarity/Console/AppTest.php
+++ tests/clarity/Console/AppTest.php
@@ -29,28 +29,24 @@ class AppTest extends \PHPUnit_Framework_TestCase
         $has_file = file_exists($file = config()->path->app.'TestModule/Routes/RouteGroup.php');
         $this->assertTrue($has_file, 'check if ['.$file.'] were generated');
 
-        if ($has_file) {
-            $file_contents = file_get_contents($file);
-            $this->assertContains('namespace App\\TestModule\\Routes;', $file_contents);
-            $this->assertContains('use Phalcon\Mvc\Router\Group as BaseRouteGroup;', $file_contents);
-            $this->assertContains('class RouteGroup extends BaseRouteGroup', $file_contents);
-        }
+        $file_contents = file_get_contents($file);
+        $this->assertContains('namespace App\\TestModule\\Routes;', $file_contents);
+        $this->assertContains('use Phalcon\Mvc\Router\Group as BaseRouteGroup;', $file_contents);
+        $this->assertContains('class RouteGroup extends BaseRouteGroup', $file_contents);
 
         $has_file = file_exists($file = config()->path->app.'TestModule/Providers/RouterServiceProvider.php');
         $this->assertTrue($has_file, 'check if ['.$file.'] were generated');
 
-        if ($has_file) {
-            $file_contents = file_get_contents($file);
-            $this->assertContains('namespace App\\TestModule\\Providers;', $file_contents);
-            $this->assertContains('use Phalcon\Di\FactoryDefault;', $file_contents);
-            $this->assertContains('use Clarity\Providers\ServiceProvider;', $file_contents);
-            $this->assertContains('use Clarity\Contracts\Providers\ModuleInterface;', $file_contents);
-            $this->assertContains('class RouterServiceProvider extends ServiceProvider implements ModuleInterface', $file_contents);
-            $this->assertContains('public function module(FactoryDefault $di)', $file_contents);
-            $this->assertContains('->setDefaultNamespace(\'App\TestModule\Controllers\');', $file_contents);
-            $this->assertContains('public function afterModuleRun()', $file_contents);
-            $this->assertContains('require_once realpath(__DIR__.\'/../\').\'/Routes.php\';', $file_contents);
-        }
+        $file_contents = file_get_contents($file);
+        $this->assertContains('namespace App\\TestModule\\Providers;', $file_contents);
+        $this->assertContains('use Phalcon\Di\FactoryDefault;', $file_contents);
+        $this->assertContains('use Clarity\Providers\ServiceProvider;', $file_contents);
+        $this->assertContains('use Clarity\Contracts\Providers\ModuleInterface;', $file_contents);
+        $this->assertContains('class RouterServiceProvider extends ServiceProvider implements ModuleInterface', $file_contents);
+        $this->assertContains('public function module(FactoryDefault $di)', $file_contents);
+        $this->assertContains('->setDefaultNamespace(\'App\TestModule\Controllers\');', $file_contents);
+        $this->assertContains('public function afterModuleRun()', $file_contents);
+        $this->assertContains('require_once realpath(__DIR__.\'/../\').\'/Routes.php\';', $file_contents);
 
         CLI::bash([
             'php brood app:route test test_module',
@@ -59,11 +55,9 @@ class AppTest extends \PHPUnit_Framework_TestCase
         $has_file = file_exists($file = config()->path->app.'TestModule/Routes/TestRoutes.php');
         $this->assertTrue($has_file, 'check if ['.$file.'] were generated');
 
-        if ($has_file) {
-            $file_contents = file_get_contents($file);
-            $this->assertContains('namespace App\\TestModule\\Routes;', $file_contents);
-            $this->assertContains('class TestRoutes extends RouteGroup', $file_contents);
-        }
+        $file_contents = file_get_contents($file);
+        $this->assertContains('namespace App\\TestModule\\Routes;', $file_contents);
+        $this->assertContains('class TestRoutes extends RouteGroup', $file_contents);
 
         CLI::bash([
             'php brood app:controller test test_module',
@@ -72,10 +66,8 @@ class AppTest extends \PHPUnit_Framework_TestCase
         $has_file = file_exists($file = config()->path->app.'TestModule/Controllers/TestController.php');
         $this->assertTrue($has_file, 'check if ['.$file.'] were generated');
 
-        if ($has_file) {
-            $file_contents = file_get_contents($file);
-            $this->assertContains('namespace App\\TestModule\\Controllers;', $file_contents);
-            $this->assertContains('class TestController extends Controller', $file_contents);
-        }
+        $file_contents = file_get_contents($file);
+        $this->assertContains('namespace App\\TestModule\\Controllers;', $file_contents);
+        $this->assertContains('class TestController extends Controller', $file_contents);
     }
 }
