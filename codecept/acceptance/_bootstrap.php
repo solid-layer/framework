0e4da52dda83f0aee7c8e15bd15f03023e5da90b
Daison Carino <daison.carino@plus65.com.sg>
2017-04-15 07:05:02 +0000

    changing codecept bootstrap in all aspects

--- codecept/acceptance/_bootstrap.php
+++ codecept/acceptance/_bootstrap.php
@@ -1,1 +1,9 @@
 <?php
+
+$path = realpath('bootstrap/start.php');
+
+if (file_exists($path) && !in_array($path, get_included_files())) {
+    require_once $path;
+
+    $kernel->modules()->run('main');
+}
--- codecept/unit/_bootstrap.php
+++ codecept/unit/_bootstrap.php
@@ -2,7 +2,7 @@
 
 $path = realpath('bootstrap/start.php');
 
-if (file_exists($path)) {
+if (file_exists($path) && !in_array($path, get_included_files())) {
     require_once $path;
 
     $kernel->modules()->run('main');
