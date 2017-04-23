3073be24e55b8d317b8ead5d0676fe5d3ddfbf90
Daison Carino <daison.carino@plus65.com.sg>
2017-04-23 05:30:53 +0000

    updating mail

--- codecept/unit/MailTest.php
+++ codecept/unit/MailTest.php
@@ -2,7 +2,9 @@
 
 use Clarity\Mail\Mail;
 use Clarity\Mail\Mailgun\Mailgun;
-use Clarity\Mail\SwiftMailer\SwiftMailer;
+use Clarity\Mail\SwiftMailer\SmtpMailer;
+use Clarity\Mail\SwiftMailer\SendmailMailer;
+use Clarity\Mail\SwiftMailer\MailMailer;
 
 class MailTest extends \Codeception\Test\Unit
 {
@@ -26,24 +28,29 @@ class MailTest extends \Codeception\Test\Unit
 
     public function testAdapters()
     {
+        $default = [
+            'host'       => env('MAILER_HOST'),
+            'port'       => env('MAILER_PORT'),
+            'username'   => env('MAILER_USERNAME'),
+            'password'   => env('MAILER_PASSWORD'),
+            'encryption' => env('MAILER_ENCRYPTION', 'tls'),
+            'from'       => env('MAILER_MAIL_FROM'),
+        ];
+
         $adapters = [
-            Mailgun::class => [
-                'from' => env('MAILER_MAIL_FROM'),
-            ],
-            SwiftMailer::class => [
-                'host'       => env('MAILER_HOST'),
-                'port'       => env('MAILER_PORT'),
-                'username'   => env('MAILER_USERNAME'),
-                'password'   => env('MAILER_PASSWORD'),
-                'encryption' => env('MAILER_ENCRYPTION', 'tls'),
-                'from'       => env('MAILER_MAIL_FROM'),
-            ],
+            // SendmailMailer::class => $default,
+            // MailMailer::class => $default,
+            // SmtpMailer::class => $default,
+            // Mailgun::class => ['from' => env('MAILER_MAIL_FROM')],
         ];
 
         foreach ($adapters as $class => $options) {
             $adapter = $this->getMailInstance($class, $options);
-            $adapter->send('welcome', [], function ($mail) {
 
+            $adapter->send('welcome', [], function ($mail) {
+                $mail->subject('Codeception Test');
+                $mail->from('daison12006013@gmail.com');
+                $mail->to(['daison12006013@gmail.com']);
             });
         }
     }
